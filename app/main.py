19e0e5553026557087a81501cd7e9715f8f440b0
Kasper Primdal Lauritzen <git@kplauritzen.dk>
2024-05-18 10:58:45 +0200

    state 2?

--- app/main.py
+++ app/main.py
@@ -3,13 +3,11 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Server is running on port 4221")
+    client_socket, addr = server_socket.accept()
+    print(f"Connection from {addr}")
+    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
